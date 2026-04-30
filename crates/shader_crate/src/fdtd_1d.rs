b1a8b579e2a84a6d7b55c5490acb837868c32bea
Nilson Sampath J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-04-30 12:25:56 -0700

    FFT shader kernels

--- crates/shader_crate/src/fdtd_1d.rs
+++ crates/shader_crate/src/fdtd_1d.rs
@@ -1,9 +1,9 @@
+use crate::select_val;
 use bytemuck::{Pod, Zeroable};
-use khal_std::glamx::UVec3;
+use khal_std::glamx::{UVec3, Vec2, Vec4, Vec4Swizzles};
 use khal_std::macros::{spirv, spirv_bindgen};
 use khal_std::num_traits::Float;
 use khal_std::sync::workgroup_memory_barrier_with_group_sync;
-use crate::select_val;
 
 #[spirv_bindgen]
 #[spirv(compute(threads(64)))]
@@ -64,6 +64,59 @@ pub fn fdtd_1d(
         src.curr_idx += source_not_finished;
     }
     workgroup_memory_barrier_with_group_sync()
+}
+
+#[spirv_bindgen]
+#[spirv(compute(threads(64)))]
+pub fn compute_fft_kernels_1d(
+    #[spirv(global_invocation_id)] id: UVec3,
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] fft_kernels: &mut [Vec4],
+    #[spirv(uniform, descriptor_set = 0, binding = 1)] fft: &FftDataGPU,
+    #[spirv(uniform, descriptor_set = 0, binding = 2)] grid: &GridInfo1D,
+) {
+    let i = id.x as usize;
+    if i >= fft_kernels.len() { return; }
+
+    let f0 = fft.f_start + fft.f_increment * i as f32;
+    let f1 = f0 + fft.f_increment;
+
+    let (f0_im, f0_re) = Float::sin_cos(-core::f32::consts::TAU * f0 * grid.dt);
+    let (f1_im, f1_re) = Float::sin_cos(-core::f32::consts::TAU * f1 * grid.dt);
+    fft_kernels[i] = Vec4::new(f0_re, f0_im, f1_re, f1_im);
+}
+
+#[spirv_bindgen]
+#[spirv(compute(threads(64)))]
+pub fn fft_1d(
+    #[spirv(global_invocation_id)] id: UVec3,
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] cells: &mut [GridCell1D],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] reflectance_fft: &mut [Vec4],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 2)] transmittance_fft: &mut [Vec4],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 3)] fft_kernels: &mut [Vec4],
+) {
+    let i0 = id.x as usize;
+    if i0 >= fft_kernels.len() { return; }
+
+    let reflected = cells[0].e_y;
+    let transmitted = cells[cells.len()-1].e_y;
+
+    reflectance_fft[i0] += fft_kernels[i0] * reflected;
+    transmittance_fft[i0] += fft_kernels[i0] * transmitted;
+}
+
+#[spirv_bindgen]
+#[spirv(compute(threads(64)))]
+pub fn finish_fft_1d(
+    #[spirv(global_invocation_id)] id: UVec3,
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] reflectance_fft: &mut [Vec4],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] transmittance_fft: &mut [Vec4],
+    #[spirv(uniform, descriptor_set = 0, binding = 2)] grid: &GridInfo1D,
+) {
+    let i = id.x as usize;
+    if i >= reflectance_fft.len() { return; }
+
+    reflectance_fft[i] *= grid.dt;
+    transmittance_fft[i] *= grid.dt;
 }
 
 #[derive(Copy, Clone, Pod, Zeroable, Default, Debug)]
@@ -145,6 +198,13 @@ pub struct GridCell1D {
     pub e_y: f32,
     pub hn_x: f32,
     pub material_idx: u32,
+}
+
+#[derive(Copy, Clone, Pod, Zeroable, Default)]
+#[repr(C)]
+pub struct FftDataGPU {
+    pub f_start: f32,
+    pub f_increment: f32,
 }
 
 #[derive(Copy, Clone, Pod, Zeroable, Default)]
