897727a03ea8b624027b58d573ec34d27e0a5a00
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-05-05 18:32:03 -0700

    Use max timesteps as DFT resolution

--- crates/graphics_testing/src/shader/run_fdtd_1d.rs
+++ crates/graphics_testing/src/shader/run_fdtd_1d.rs
@@ -48,9 +48,8 @@ pub async fn run_fdtd_1d(backend: &GpuBackend) {
     data.prepare_for_gpu(&stability_values, None);
 
     let mut f_res = data.estimate_max_timesteps(None);
-        if f_res % 2 == 0 { f_res -= 1; }
     let f_max = max_pulse_freq;
-    data.enable_dfts(0.0..=f_max, 10001);
+    data.enable_dfts(0.0..=f_max, f_res);
 
     data.set_step_count(1);
     println!("{:?}", data.grid_info);
