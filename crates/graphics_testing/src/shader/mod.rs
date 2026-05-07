20d96a6d40f8a86ff1094c76278075bece88814b
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-05-06 21:54:09 -0700

    Remove 3D Fdtd code
    It's too ambitious to be going for 3D Fdtd right now. For now we focus on 2D.

--- crates/graphics_testing/src/shader/mod.rs
+++ crates/graphics_testing/src/shader/mod.rs
@@ -1,2 +1,1 @@
 pub mod run_fdtd1;
-pub mod run_fdtd_3d;
--- crates/shader_crate/src/lib.rs
+++ crates/shader_crate/src/lib.rs
@@ -5,7 +5,6 @@ use bytemuck::{Pod, Zeroable};
 use khal_std::num_traits::Float;
 
 pub mod fdtd1;
-pub mod fdtd_3d;
 
 /// Select between two numerical values `tru` and `fals` of type `typ` depending on a bool `b`.
 ///
